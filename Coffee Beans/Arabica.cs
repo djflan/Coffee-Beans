05e8ec035c7e31030ac14ec6355264904275f900
Dan Flanigan <djflan@gmail.com>
2022-07-27 14:12:59 -0500

    added Arabica

---
+++ Coffee Beans/Arabica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coffee_Beans
+{
+    public class Arabica : Bean
+    {
+        public Arabica()
+        {
+            Name = "Arabica";
+        }
+    }
+}
